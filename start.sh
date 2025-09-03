9cafa6fec7eaa592ca93d2bb15fffbc7df9bfae7
AbhishekSinghDIkhit <aabhishek576@gmail.com>
2025-09-03 22:52:39 +0530

    update

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Install Playwright dependencies (Chromium + system libs)
-playwright install --with-deps chromium
+# Install Playwright dependencies (Chromium)
+playwright install chromium
 
 # Run FastAPI app
 exec uvicorn main:app --host 0.0.0.0 --port $PORT
